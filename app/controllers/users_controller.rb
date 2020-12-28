2ddbdfbe5e7b8e41644ecb3147864fc7867ff442
daviidy <yaodavidarmel@gmail.com>
2020-12-28 18:12:59 +0000

    fix user issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+      session[:user_id] = @user.id
+      redirect_to root_path
     else
       render :new
     end
